7f5f420b12977c0eec1eaef709838956b3a8eadd
CHARAN422176 <422176@student.nitandhra.ac.in>
2025-06-24 13:54:01 +0530

    commit

--- tokenizer_wrapper.py
+++ tokenizer_wrapper.py
@@ -1,9 +1,9 @@
 import pandas as pd
 # from tensorflow.python.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
 # from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.python.keras.preprocessing.text import text_to_word_sequence
+# from tensorflow.python.keras.preprocessing.text import text_to_word_sequence
 import numpy as np
 from transformers import GPT2Tokenizer
 
